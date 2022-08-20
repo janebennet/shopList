f9b8865585bd69464841d4cc1027234ee5e41fb4
janebennet <kulik.hanna@gmail.com>
2022-08-19 23:03:05 -0700

    Final working version of the shopping list

--- printList.java
+++ printList.java
@@ -1,27 +1,38 @@
 package shopList;
 
-import java.io.BufferedReader;
+import java.io.*;
 import java.io.File;
-import java.io.FileReader;  
-public class printList  
+import java.io.FileReader;
 
-{  
-public static void main(String[] args)   
-{  
-String line = "";  
-String splitBy = ",";  
-String filePath = System.getProperty("user.dir") + "/shopList/ShoppingList.csv";
+public class printList
 
- 
-FileReader reader = new FileReader("/shopList/ShoppingList.csv");
-//parsing a CSV file into BufferedReader class constructor  
-BufferedReader br = new BufferedReader(new FileReader(filePath));  
-while (null != null)   //returns a Boolean value  
-{  
-String[] item = line.split(splitBy);    // use comma as separator  
-System.out.println("Item name" + item[0] + ", number =" + item[1]);  
-}  
-}   
- 
-}  
+{
+    public static void main(String[] args) {
+        String line = "";
+        String splitBy = ",";
+        String filePath = System.getProperty("user.dir") + "/shopList/ShoppingList.csv";
 
+        File file = new File(filePath);
+        if (!file.exists() || file.isDirectory()) {
+            System.out.println("File does not exist");
+            System.exit(-1);
+        }
+        // parsing a CSV file into BufferedReader class constructor
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            Integer counter = 0;
+            while ((line = br.readLine()) != null) // returns a Boolean value
+            {
+                counter++;
+                String[] item = line.split(splitBy); // use comma as separator
+                System.out.println(counter + ": " + item[0] + " - " + item[1]);
+            }
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found" + e);
+        } catch (IOException ioe) {
+            System.out.println("IO exception" + ioe);
+        }
+    }
+}
